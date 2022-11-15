a59821f4a21a4b9123e93bcf8622882808c9f185
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-11-15 13:19:48 -0500

    agregando nuevos archivos y vinculando archivos js

---
+++ js/analisis.js
@@ -0,0 +1,16 @@
+const persona = document.querySelector("#persona");
+const btnpersona = document.querySelector("#btn-persona");
+btnpersona.addEventListener("click", enviarPersona);
+let consulta;
+
+class AnalisisPlatzi {
+  static buscarPersona(consulta) {
+    return salarios.find((sal) => sal.name === consulta);
+  }
+}
+
+function enviarPersona() {
+  consulta = persona.value;
+}
+
+console.log(arrSueldos)
--- js/estadistica.js
+++ js/estadistica.js
@@ -1,5 +1,6 @@
+const PlatziMath = {}
 const salario = document.querySelector("#salario");
-const salarios = document.querySelector("#salarios");
+const sueldos = document.querySelector("#sueldos");
 const promedio = document.querySelector("#promedio");
 const mediana = document.querySelector("#mediana");
 const moda = document.querySelector("#moda");
@@ -12,62 +13,12 @@ btnCalcularPromedio.addEventListener("click", fnCalcularPromedio);
 btnCalcularMediana.addEventListener("click", fnCalcularMediana);
 btnCalcularModa.addEventListener("click", fnCalcularModa);
 
+
 let arrSueldos = [];
-
-class PlatziMath {
-
-  static calcularPromedio(lista) {
-    const mediaAritmetica =
-      lista.reduce((ante, actu) => {
-        return ante + actu;
-      }) / lista.length;
-    return `El promedio es ${mediaAritmetica.toFixed(2)}`;
-  }
-
-  static esPar(arr) {
-    return !(arr.length % 2);
-  }
-
-  static calcularMediana(lista) {
-    let calculoMediana;
-    lista.sort((a, b) => a - b); // Aqui es donde ordeno el arreglo
-    if (PlatziMath.esPar(lista)) {
-      valor1 = lista.length / 2;
-      valor2 = lista.length / 2 - 1;
-      calculoMediana = (lista[valor1] + lista[valor2]) / 2;
-      return `La mediana es ${calculoMediana}`;
-    } else {
-      calculoMediana = lista[Math.floor(lista.length / 2)];
-      return `La mediana es ${calculoMediana}`;
-    }
-  }
-
-  static calcularModa(listas) {
-    const cuenta = {};
-
-    listas.forEach((lista) => {
-      if (cuenta[lista]) {
-        cuenta[lista] += 1;
-      } else {
-        cuenta[lista] = 1;
-      }
-    });
-    const cuentaArr = Object.entries(cuenta);
-
-    cuentaArr.sort((a, b) => b[1] - a[1]);
-
-    if (cuentaArr[0][1] === cuentaArr[1][1]) {
-      return `No hay moda`;
-    } else {
-      return `La moda es: ${cuentaArr[0][0]} y la cantidad de veces fue ${cuentaArr[0][1]}`;
-    }
-  }
-  
-}
 
 function enviarSueldo() {
   arrSueldos.push(parseInt(salario.value));
-  salarios.innerHTML += `${salario.value} `;
+  sueldos.innerHTML += `${salario.value} `;
 }
 
 function fnCalcularPromedio() {
@@ -83,4 +34,51 @@ function fnCalcularMediana() {
 function fnCalcularModa() {
   let outPut = PlatziMath.calcularModa(arrSueldos);
   moda.innerHTML = outPut;
+}
+
+PlatziMath.calcularPromedio = function calcularPromedio(lista) {
+  const mediaAritmetica =
+    lista.reduce((ante, actu) => {
+      return ante + actu;
+    }) / lista.length;
+  return `El promedio es ${mediaAritmetica.toFixed(2)}`;
+}
+
+PlatziMath.esPar = function esPar(arr) {
+  return !(arr.length % 2);
+}
+
+PlatziMath.calcularMediana = function calcularMediana(lista) {
+  let calculoMediana;
+  lista.sort((a, b) => a - b); // Aqui es donde ordeno el arreglo
+  if (PlatziMath.esPar(lista)) {
+    valor1 = lista.length / 2;
+    valor2 = lista.length / 2 - 1;
+    calculoMediana = (lista[valor1] + lista[valor2]) / 2;
+    return `La mediana es ${calculoMediana}`;
+  } else {
+    calculoMediana = lista[Math.floor(lista.length / 2)];
+    return `La mediana es ${calculoMediana}`;
+  }
+}
+
+PlatziMath.calcularModa = function calcularModa(listas) {
+  const cuenta = {};
+
+  listas.forEach((lista) => {
+    if (cuenta[lista]) {
+      cuenta[lista] += 1;
+    } else {
+      cuenta[lista] = 1;
+    }
+  });
+  const cuentaArr = Object.entries(cuenta);
+
+  cuentaArr.sort((a, b) => b[1] - a[1]);
+
+  if (cuentaArr[0][1] === cuentaArr[1][1]) {
+    return `No hay moda`;
+  } else {
+    return `La moda es: ${cuentaArr[0][0]} y la cantidad de veces fue ${cuentaArr[0][1]}`;
+  }
 }
